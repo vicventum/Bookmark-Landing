a5b07b09962e8c7b1202fee580987ba3ea3dedd2
Víctor Álvarez <victorpool22@gmail.com>
2020-09-19 14:24:53 -0400

    Agrega archivos iniciales

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// vendor
+import "../node_modules/materialize-css/js/tabs"
+import "../node_modules/materialize-css/js/collapsible"
+
+// Import CSS
+import "./app/scss/main.scss";
+
+// Import JavaScript
+// import "./app/js/vendor/hamburger";
+import "./app/js/app";
