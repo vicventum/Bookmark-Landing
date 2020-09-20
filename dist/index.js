bff88b55e2084bd2b7b832ecf830c90c638b28e1
Víctor Álvarez <victorpool22@gmail.com>
2020-09-20 18:03:41 -0400

    Termina nav en mobile

--- dist/index.js
+++ dist/index.js
@@ -184,45 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"app/vendor/v-grid.min.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"app/scss/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"../vendor/v-grid.min.css":"app/vendor/v-grid.min.css","_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"app/vendor/hamburger-config.js":[function(require,module,exports) {
-// Look for .hamburger
-var hamburger = document.getElementById("iconHamburger"),
-    header = document.getElementById('header'),
-    // body = document.getElementById('body'),
-links = document.querySelectorAll('.nav__link'); // On click
-
-hamburger.addEventListener("click", function () {
-  // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
-  body.classList.toggle("no-scroll"); // header.classList.toggle("open");
-});
-links.forEach(function (link) {
-  link.addEventListener('click', function () {
-    hamburger.classList.remove("is-active");
-    body.classList.remove("no-scroll"); // header.classList.remove("open");
-  });
-});
-},{}],"app/js/app.js":[function(require,module,exports) {
-
-},{}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./app/scss/main.scss");
-
-require("./app/vendor/hamburger-config");
-
-require("./app/js/app");
-},{"./app/scss/main.scss":"app/scss/main.scss","./app/vendor/hamburger-config":"app/vendor/hamburger-config.js","./app/js/app":"app/js/app.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -426,5 +388,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/index.js.map
--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@
 import "./app/scss/main.scss";
 
 // Import JavaScript
-// import "./app/js/vendor/hamburger";
+import "./app/vendor/hamburger-config";
 import "./app/js/app";
